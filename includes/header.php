f21ec81bd1fa78b18e927e76e2fe59e4c6a99f1f
tomwicks <TomW@Pans-MacBook-Pro.local>
2017-01-29 22:58:18 +0000

    Twitter & Facebook links open in new window

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
 		<a class="logo" href="index"><img src="images/logo.svg"></a>
 		<nav>
 			<ul>
-				<li><a href="https://www.facebook.com/MyTalkingPet/" class="facebook"></a></li>
-				<li><a href="https://twitter.com/mytalkingpet" class="twitter"></a></li>
+				<li><a target="_blank" href="https://www.facebook.com/MyTalkingPet/" class="facebook"></a></li>
+				<li><a target="_blank" href="https://twitter.com/mytalkingpet" class="twitter"></a></li>
 				<li><a href="mailto:hello@wobamedia.info" class="email">Email Us</a></li>
 			</ul>
 		</nav>
